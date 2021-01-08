39523d9de9f252b15a8f1480dd5ffedb62b2b617
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-01-08 14:51:32 +0000

    elaboraçao de conteudo na pagina index

---
+++ Javascript/JavaScript.js
@@ -0,0 +1,17 @@
+﻿
+var imgAtual = 1;
+function MudaImg() {
+    var img = imgAtual + 1;
+    if (img === 4) img = 1;
+    SeleccionaImagem(img);
+}
+
+window.setInterval("MudaImg()", 2000);
+
+function SeleccionaImagem(num) {
+    document.getElementById("imggrd").src = "images/" + num + ".jpg";
+    imgAtual = num;
+}
+
+
+/*Som*/
